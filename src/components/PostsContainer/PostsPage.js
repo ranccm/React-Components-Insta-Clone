86a8d65f48c1042b9537f16cc9a065e478851c8d
ranccm <ranimnapier@gmail.com>
2020-02-18 17:52:55 -0800

    post container complete. working on comment section

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,24 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-
+import dummyData from "../../dummy-data"
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState(dummyData);
+  console.log(data);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(item => (
+        <Post 
+          post = {item} 
+        />
+    
+      ))}
     </div>
   );
 };
